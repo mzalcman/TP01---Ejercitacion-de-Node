0dc579a74f619e0e1e3ba1d8bb3d8ba3178f9829
Android Studio <devandroid@ort>
2026-03-16 09:05:42 -0300

    Subir el proyecto

---
+++ src/ej01.js
@@ -0,0 +1,16 @@
+let textoEntrada01 = "Escuela", textoEntrada02 = "ORT";
+let textoSalida = "";
+
+textoSalida = concatInvert(textoEntrada01, textoEntrada02);
+
+console.clear(); // Borra la pantalla en la consola.
+console.log(`Textos de Entrada: "${textoEntrada01}" y "${textoEntrada02}"`);
+console.log(`Texto de Salida: "${textoSalida}"`);
+
+function concatInvert(texto1, texto2) {
+    let returnValue = '';
+    let strCaracteresDeLasPalabrasConcatenados = texto1.split("").concat(texto2.split(""));
+    let chrArrayDePalabrasInvertido = strCaracteresDeLasPalabrasConcatenados.reverse();
+    returnValue = chrArrayDePalabrasInvertido.join("")
+    return returnValue;
+}
